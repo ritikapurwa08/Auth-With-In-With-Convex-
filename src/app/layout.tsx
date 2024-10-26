ca9b836587fcdcf6f2074829ce53c6897985d2af
ritik-apurwa08 <8ballpoolrk2@gmail.com>
2024-10-26 13:42:16 +0530

    Initial commit from Create Next App and i am learning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,9 @@
+import React from "react";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
+import { ConvexClientProvider } from "@/components/providers/convex-client-provider";
+import JotaiProvider from "@/components/providers/jotai-provider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,12 +16,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ConvexAuthNextjsServerProvider>
+      <ConvexClientProvider>
+        <JotaiProvider>
+          <html lang="en">
+            <body className={` antialiased`}>{children}</body>
+          </html>
+        </JotaiProvider>
+      </ConvexClientProvider>
+    </ConvexAuthNextjsServerProvider>
   );
 }
+
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
